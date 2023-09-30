fe44c234e499674d3d7bc32424463b6e5acba590
LokiSonOfOdin <95542800+LokiSonOfOdin@users.noreply.github.com>
2023-09-30 09:49:50 +0500

    add project

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+
+
+new Swiper(".swipers", {
+    spaceBetween: 15,
+    direction: "horizontal",
+    loop: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+  });
+  
+  
